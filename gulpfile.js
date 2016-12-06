02a50eac9745d0f30661cbbc45db65ed71a2d81f
Wcc723 <agito723@gmail.com>
2016-12-06 16:00:39 +0800

    add others and gulp default

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,47 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var autoprefixer = require('autoprefixer');
-var postcss = require('gulp-postcss');
+var gulp = require('gulp'),
+  sass = require('gulp-sass'),
+  autoprefixer = require('autoprefixer'),
+  plumber = require('gulp-plumber'),
+  postcss = require('gulp-postcss');
+
+var path = {
+  source: './source/',
+  public: './public/'
+}
 
 gulp.task('sass', function () {
   var processors = [
       autoprefixer({browsers: ['last 5 version']})
   ];
-  return gulp.src('./source/scss/**/*.scss')
+  return gulp.src(path.source + 'scss/**/*.scss')
+    .pipe(plumber())
     .pipe(sass(
       {outputStyle: 'expanded'}
     ).on('error', sass.logError))
     .pipe(postcss(processors))
-    .pipe(gulp.dest('./public/stylesheets'));
+    .pipe(gulp.dest(path.public + 'stylesheets'));
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./sass/**/*.scss', ['sass']);
+// 其它不編譯的物件
+var objs = ['./source/**/**.*'];
+var others = [
+    '**/*.scss',
+    '**/*.sass',];
+for (var i = 0; i < others.length; i++) {
+  objs.push('!' + path.source + others[i]);
+}
+gulp.task('others', function(){
+  return gulp.src(objs)
+    .pipe(plumber())
+    .pipe(gulp.dest(path.public));
 });
+// watch(objs, function() {
+//   gulp.start('others');
+// });
+
+gulp.task('watch', function () {
+  gulp.watch('./sass/**/*.scss', ['sass']);
+  gulp.watch(objs, ['others']);
+});
+
+gulp.task('default', ['others', 'sass', 'watch']);
