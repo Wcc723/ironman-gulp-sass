07c7317627542aa196978f8d0ae3223983b4ef2d
Wcc723 <agito723@gmail.com>
2016-12-06 15:38:12 +0800

    add autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,17 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var autoprefixer = require('autoprefixer');
+var postcss = require('gulp-postcss');
 
 gulp.task('sass', function () {
+  var processors = [
+      autoprefixer({browsers: ['last 5 version']})
+  ];
   return gulp.src('./source/scss/**/*.scss')
     .pipe(sass(
       {outputStyle: 'expanded'}
     ).on('error', sass.logError))
+    .pipe(postcss(processors))
     .pipe(gulp.dest('./public/stylesheets'));
 });
 
